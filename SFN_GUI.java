c700310ae50c4a508c1fb40180adc41e1c6c076b
Bishesh Manandhar <33968428+bisheshm@users.noreply.github.com>
2017-11-28 13:03:17 -0600

    Add files via upload

---
+++ SFN_GUI.java
@@ -0,0 +1,33 @@
+/**
+ * @(#)SFN_GUI.java
+ *
+ * SFN_GUI application
+ *
+ * @author
+ * @version 1.00 2017/3/26
+ */
+import javax.swing.JOptionPane;
+public class SFN_GUI {
+
+    public static void main(String[] args) {
+
+    	String name = JOptionPane.showInputDialog("What is your name?");
+    	String email = JOptionPane.showInputDialog("What is your email address?");
+
+    	Object[] possibleValues = { "1", "2", "3", "4", "5" };
+		Object selectedValue = JOptionPane.showInputDialog(null,"How many pairs of cleats will you be donating?", "Input",JOptionPane.INFORMATION_MESSAGE, null, possibleValues, possibleValues[0]);
+
+    	int reply = JOptionPane.showConfirmDialog(null, "Is the following information correct? \nMy name is " + name + "\n" + email + "\n" + "I will be donating " + selectedValue + " pair(s) of cleats", "Choose One", JOptionPane.YES_NO_OPTION);
+
+		if (reply == 0) {
+			JOptionPane.showMessageDialog(null, "Thank You!");
+		}
+
+		if (reply == 1) {
+			JOptionPane.showMessageDialog(null, "Please fill out this form again!");
+
+		}
+    }
+
+}
+
