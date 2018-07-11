982136bc065d3af7903195d6b5fc8c6566141dc2
Bishesh Manandhar <33968428+bisheshm@users.noreply.github.com>
2018-07-11 12:45:25 -0500

    Update SFN_GUI.java

--- SFN_GUI.java
+++ SFN_GUI.java
@@ -10,13 +10,16 @@ import javax.swing.JOptionPane;
 public class SFN_GUI {
 
     public static void main(String[] args) {
-
+	    
+	// Getting name and email address
     	String name = JOptionPane.showInputDialog("What is your name?");
     	String email = JOptionPane.showInputDialog("What is your email address?");
 
+	// Getting number of cleats that will be donated
     	Object[] possibleValues = { "1", "2", "3", "4", "5" };
 		Object selectedValue = JOptionPane.showInputDialog(null,"How many pairs of cleats will you be donating?", "Input",JOptionPane.INFORMATION_MESSAGE, null, possibleValues, possibleValues[0]);
-
+	
+	// Confirmation of donation
     	int reply = JOptionPane.showConfirmDialog(null, "Is the following information correct? \nMy name is " + name + "\n" + email + "\n" + "I will be donating " + selectedValue + " pair(s) of cleats", "Choose One", JOptionPane.YES_NO_OPTION);
 
 		if (reply == 0) {
